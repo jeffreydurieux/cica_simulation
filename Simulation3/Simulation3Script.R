fb88393eeafde1ff10203b613cbd986f9c4baaff
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2019-09-09 16:14:06 +0200

    changed some code to make it ready for Shark

--- Simulation3/Simulation3Script.R
+++ Simulation3/Simulation3Script.R
@@ -1,7 +1,7 @@
 #### Model Selection Beckmann networks
 # true model: 3 clusters 4 components
-# range of clusters = 1, 2, 3, 4, 5
-# range of comp     = 2, 3, 4, 5, 6
+# range of clusters = 2, 3, 4, 5  # do group icas for K = 1
+# range of comp     = 2, 3, 4, 5, 6, 7
 
 
 args <- commandArgs(TRUE)
@@ -9,7 +9,6 @@ args <- as.numeric(args)
 
 clus <- args[1]
 comp <- args[2]
-start <- args[3]
 
 set.seed(2407)
 
@@ -25,15 +24,19 @@ setwd("/exports/fsw/durieuxj/Simulations/CICA_SIM_2/Output/")
 
 #### select data set here #####
 
-X <- get(load("SubSampXE_SNR_1_Gaussian.Rdata"))
+X <- get(load("rep_1CICA_simdata_0.30.Rdata"))
 
 #### run CICA
-#tmp <- proc.time()
+tmp <- proc.time()
 cica <- CICA(nStarts = 30, DataList = X, nComp = comp, nClus = clus, show='best')
-#time <- proc.time() - tmp
+time <- proc.time() - tmp
+
+
 
 
 # output
-ext1 <- paste("clus", clus, "_comp", comp,"start", start , "_resultSim3.Rdata",sep = "")
+setwd("/exports/fsw/durieuxj/Simulations/CICA_SIM_3/")
+
+ext1 <- paste("clus", clus, "_comp", comp, "_resultSim3.Rdata",sep = "")
 save(cica, file = ext1)
 
