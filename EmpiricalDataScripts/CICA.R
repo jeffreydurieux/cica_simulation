dae8becd9772984d01624e25415f2348ce7da4c9
Jeffrey Durieux <durieux.jeffrey@gmail.com>
2020-07-13 19:44:28 +0200

    latest save

--- EmpiricalDataScripts/CICA.R
+++ EmpiricalDataScripts/CICA.R
@@ -1,14 +1,24 @@
 ### CICA P1.5 shark
 ### script used for empirical cica analysis
 
+### note: did a Q = 5 analysis on new shark slurm schedular
+#### some adjustments were made
+
 args <- commandArgs(TRUE)
 args <- as.numeric(args)
 
-clus <- args[1]
-comp <- args[2]
-start_quint <- args[3] 
+#clus <- args[1]
+#comp <- args[2]
+#start_quint <- args[3] 
 
-set.seed(start_quint)
+##### slurm grid ######
+
+grid <- expand.grid(clus = 2:5, comp = 5, start_quint = 1:30)
+row <- args[1]
+mygrid <- grid[row,]
+
+
+set.seed(mygrid$start_quint)
 
 # sources and libraries
 library(ica)
@@ -43,15 +53,16 @@ dataL <- lapply(dataL, t)
 # do cica
 tmp <- proc.time()
 #cica <- CICA(nStarts = 1, DataList = dataL, nComp = comp, nClus = clus)
-cica <- CICAStart(DataList = dataL, exSig = comp, nClus = clus, newclus = clusf(length(dataL), clus),icaf = "no")
+cica <- CICAStart(DataList = dataL, exSig = mygrid$comp, nClus = mygrid$clus, 
+                  newclus = clusf(length(dataL), clus),icaf = "no")
 time <- proc.time() - tmp
 
 # output
-setwd( paste("/exports/fsw/durieuxj/P1.5/CICAOUTPUT/clus", clus,sep = "") )
-ext1 <- paste("CICA_nc",comp, "Batch", start_quint ,".Rdata" , sep = "")
+setwd( paste("/exports/fsw/durieuxj/P1.5/CICAOUTPUT/clus", mygrid$clus,sep = "") )
+ext1 <- paste("CICA_nc", mygrid$comp, "Batch", mygrid$start_quint ,".Rdata" , sep = "")
 save(cica, file = ext1)
-ext2 <- paste("TimingCICA_nc",comp, "Batch", start_quint, ".Rdata" , sep = "")
-save(time, file = ext2)
+#ext2 <- paste("TimingCICA_nc",comp, "Batch", start_quint, ".Rdata" , sep = "")
+#save(time, file = ext2)
 
 
 
